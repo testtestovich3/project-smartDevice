504e9004203f3f2d80be57f192d3f7828211af64
Alexey <alexey.anasov@yandex.by>
2020-03-17 18:12:00 +0300

    правка js

--- source/js/main.js
+++ source/js/main.js
@@ -74,24 +74,26 @@ accordions.forEach(function (accordion) {
   var btn = accordion.querySelector('.footer__btn');
   var list = accordion.querySelector('.footer__list');
 
-  btn.addEventListener('click', function () {
-    if (btn.classList.contains('footer__btn--opened')) {
-      btn.classList.remove('footer__btn--opened');
-    } else {
-      accordionsBtn.forEach(function (accordionBtn) {
-        accordionBtn.classList.remove('footer__btn--opened');
-      });
-      btn.classList.add('footer__btn--opened');
-    }
-    if (list.classList.contains('footer__list--opened')) {
-      list.classList.remove('footer__list--opened');
-    } else {
-      accordionsList.forEach(function (accordionList) {
-        accordionList.classList.remove('footer__list--opened');
-      });
-      list.classList.add('footer__list--opened');
-    }
-  });
+  if(btn){
+    btn.addEventListener('click', function () {
+      if (btn.classList.contains('footer__btn--opened')) {
+        btn.classList.remove('footer__btn--opened');
+      } else {
+        accordionsBtn.forEach(function (accordionBtn) {
+          accordionBtn.classList.remove('footer__btn--opened');
+        });
+        btn.classList.add('footer__btn--opened');
+      }
+      if (list.classList.contains('footer__list--opened')) {
+        list.classList.remove('footer__list--opened');
+      } else {
+        accordionsList.forEach(function (accordionList) {
+          accordionList.classList.remove('footer__list--opened');
+        });
+        list.classList.add('footer__list--opened');
+      }
+    });
+  };
 });
 
 // Скрол
